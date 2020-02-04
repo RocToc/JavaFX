8e884482a861534d010fb6e6c117cde62a497618
Timofey Shelove <Timjacke@gmail.com>
2020-02-04 18:50:36 +0300

    tictactoe

---
+++ src/sample/TicTacToeController.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class TicTacToeController {
+    @FXML
+    Button button11;
+    @FXML
+    Button button12;
+    @FXML
+    Button button13;
+    @FXML
+    Button button21;
+    @FXML
+    Button button22;
+    @FXML
+    Button button23;
+    @FXML
+    Button button31;
+    @FXML
+    Button button32;
+    @FXML
+    Button button33;
+
+
+
+}
+
