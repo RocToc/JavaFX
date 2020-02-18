26eee44a74f2437bfff9bcb6c0caf8403d5ba17c
Timofey Shelove <Timjacke@gmail.com>
2020-02-18 18:14:45 +0300

    partly full

--- src/tictactoe/TicTacToeController.java
+++ src/tictactoe/TicTacToeController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicTacToeController {
     @FXML
     Button button11;
@@ -27,8 +30,6 @@ public class TicTacToeController {
 
     TicTacToeModel model = new TicTacToeModel();
 
-
-
     public void onPressButton(MouseEvent event) {
         Button source = (Button) event.getSource();
         if (source.getText().equals("")) {
@@ -36,5 +37,19 @@ public class TicTacToeController {
             source.setText(model.getPlayerSymbol());
         }
 
+        List<String> positionValues = new ArrayList<>();
+        positionValues.add(0, button11.getText());
+        positionValues.add(1, button12.getText());
+        positionValues.add(2, button13.getText());
+        positionValues.add(3, button21.getText());
+        positionValues.add(4, button22.getText());
+        positionValues.add(5, button23.getText());
+        positionValues.add(6, button31.getText());
+        positionValues.add(7, button32.getText());
+        positionValues.add(8, button33.getText());
+        String winner = model.getWinnerIfExist(positionValues);
+    }
+    public String winner(Event event) {
+        if ()
     }
 }
--- src/tictactoe/TicTacToeModel.java
+++ src/tictactoe/TicTacToeModel.java
@@ -17,12 +17,42 @@ public class TicTacToeModel {
             return "X";
         }
     }
-    void noChangeSymbol() {
-        if (symbol.equals("X")) {
-            symbol.equals(symbol);
+    public String getWinnerIfExist(List<String> positionValues) {
+        String winner = "";
+        if (positionValues.get(0).equals(positionValues.get(1))
+            && positionValues.get(2).equals(positionValues.get(1))
+            && positionValues.get(0).equals(positionValues.get(2))) {
+            winner = positionValues.get(0);
+        } else if (positionValues.get(3).equals(positionValues.get(4))
+                && positionValues.get(5).equals(positionValues.get(4))
+                && positionValues.get(3).equals(positionValues.get(5))) {
+            winner = positionValues.get(3);
+        } else if (positionValues.get(6).equals(positionValues.get(7))
+                && positionValues.get(8).equals(positionValues.get(7))
+                && positionValues.get(6).equals(positionValues.get(8))) {
+            winner = positionValues.get(6);
+        } else if (positionValues.get(0).equals(positionValues.get(3))
+                && positionValues.get(6).equals(positionValues.get(3))
+                && positionValues.get(0).equals(positionValues.get(6))) {
+            winner = positionValues.get(0);
+        } else if (positionValues.get(1).equals(positionValues.get(4))
+                && positionValues.get(7).equals(positionValues.get(4))
+                && positionValues.get(1).equals(positionValues.get(7))) {
+            winner = positionValues.get(1);
+        } else if (positionValues.get(2).equals(positionValues.get(5))
+                && positionValues.get(8).equals(positionValues.get(5))
+                && positionValues.get(2).equals(positionValues.get(8))) {
+            winner = positionValues.get(2);
+        } else if (positionValues.get(0).equals(positionValues.get(4))
+                && positionValues.get(8).equals(positionValues.get(4))
+                && positionValues.get(0).equals(positionValues.get(8))) {
+            winner = positionValues.get(0);
+        } else if (positionValues.get(2).equals(positionValues.get(4))
+                && positionValues.get(6).equals(positionValues.get(4))
+                && positionValues.get(2).equals(positionValues.get(6))) {
+            winner = positionValues.get(2);
         }
-    }
-    public void getWinnerIfExist(List<String> positionValues) {
 
+        return winner;
     }
 }
